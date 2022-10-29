fc3cca2645282a49c7acb1db5e73bbb04a3f42f0
ThomasThings <89316672+ThomasThings@users.noreply.github.com>
2022-10-29 06:17:56 -0400

    Update and rename src/example/ExampleJavaMod.java to src/prime/ExampleJavaMod.java

--- src/prime/ExampleJavaMod.java
+++ src/prime/ExampleJavaMod.java
@@ -1,4 +1,4 @@
-package example;
+package prime;
 
 import arc.*;
 import arc.util.*;
@@ -9,19 +9,19 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class PrimeMod extends Mod{
 
     public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+        Log.info("Loaded PrimeMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("among-us");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("prime-sussy")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +30,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some prime content.");
     }
 
 }
