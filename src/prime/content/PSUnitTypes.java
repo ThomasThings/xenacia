7740f895b29141fde590d5a255987c611dd62274
ThomasThings <TheYTFurryGamer@gmail.com>
2022-11-07 19:51:16 -0500

    fixing units again

--- src/prime/content/PSUnitTypes.java
+++ src/prime/content/PSUnitTypes.java
@@ -16,13 +16,19 @@ public class PSUnitTypes {
             health = 100f;
             armor = 0f;
             speed = 1f;
+            drag = 0.11f;
 
+            legForwardScl = 2f,
+            legContinuousMove = true;
             legCount = 4;
             legGroupSize = 1;
             legLength = 6f;
             legExtension = 1.5f;
             legBaseOffset = 0f;
             legStraightness = 0.5f;
+            rippleScale = 0f;
+            legMaxLength = 1f;
+            legMinLength = 1f;
         }};
     }
 }
