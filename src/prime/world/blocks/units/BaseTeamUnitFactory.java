e455cdd274317a47c2dd0ab3de2f1fa74684176e
ThomasThings <TheYTFurryGamer@gmail.com>
2022-11-11 23:30:00 -0500

    Update BaseTeamUnitFactory.java

--- src/prime/world/blocks/units/BaseTeamUnitFactory.java
+++ src/prime/world/blocks/units/BaseTeamUnitFactory.java
@@ -32,7 +32,7 @@ public class BaseTeamUnitFactory extends UnitFactory{
         public void draw(){
             super.draw();
 
-            Draw.z(Layer.blockOver - 0.2f);
+            Draw.z(Layer.blockOver - 1f);
 
             Draw.color(team.color);
             Draw.rect(teamRegion, x, y);
