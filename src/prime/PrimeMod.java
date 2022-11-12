8f790e8c3eebb7462662b6720f0b982d52c809a8
ThomasThings <TheYTFurryGamer@gmail.com>
2022-11-11 21:27:29 -0500

    Update PrimeMod.java

--- src/prime/PrimeMod.java
+++ src/prime/PrimeMod.java
@@ -2,18 +2,17 @@ package prime;
 
 import mindustry.mod.*;
 import prime.content.*;
-import prime.world.blocks.units.BaseTeamUnitFactory;
 
 public class PrimeMod extends Mod{
 
     @Override
+
     public void loadContent(){
         PSItems.load();
         PSLiquids.load();
         PSWalls.load();
         PSEnvironment.load();
         PSUnitTypes.load();
-        BaseTeamUnitFactory.load();
         PSUnitFactories.load();
     }
 
