35a2ae7eaa5fe076306a2806e8d44ec59fb59bab
ThomasThings <TheYTFurryGamer@gmail.com>
2022-11-27 15:12:46 -0500

    Update XenStorage.java

--- src/xenacia/content/XenStorage.java
+++ src/xenacia/content/XenStorage.java
@@ -1,21 +1,17 @@
 package xenacia.content;
 
-import arc.graphics.Color;
 import mindustry.type.*;
 import mindustry.world.*;
-import mindustry.world.blocks.distribution.DirectionLiquidBridge;
-import mindustry.world.blocks.liquid.*;
 import mindustry.content.*;
 import mindustry.world.blocks.storage.StorageBlock;
-import xenacia.world.blocks.fluids.TeamLiquidRouter;
 
 import static mindustry.type.ItemStack.*;
 
 public class XenStorage{
     public static Block
             smallVault, largeVault, bulkVault, resourcePayloadTransportVault;
-    public static void load(){
-        smallVault = new StorageBlock("small-vault"){{
+    public static void load() {
+        smallVault = new StorageBlock("small-vault") {{
             requirements(Category.effect, with(XenItems.iron, 100, Items.graphite, 50));
             health = 480;
             size = 2;
@@ -23,7 +19,7 @@ public class XenStorage{
 
             researchCostMultiplier = 1f;
         }};
-        largeVault = new StorageBlock("large-vault"){{
+        largeVault = new StorageBlock("large-vault") {{
             requirements(Category.effect, with(XenItems.iron, 300, Items.titanium, 250));
             health = 1080;
             size = 3;
@@ -31,7 +27,7 @@ public class XenStorage{
 
             researchCostMultiplier = 1f;
         }};
-        bulkVault = new StorageBlock("large-vault"){{
+        bulkVault = new StorageBlock("large-vault") {{
             requirements(Category.effect, with(XenItems.cobalt, 500, XenItems.iron, 500, Items.titanium, 350));
             health = 3000;
             size = 5;
@@ -39,7 +35,7 @@ public class XenStorage{
 
             researchCostMultiplier = 1f;
         }};
-        resourcePayloadTransportVault = new StorageBlock("resource-payload-transport-vault"){{
+        resourcePayloadTransportVault = new StorageBlock("resource-payload-transport-vault") {{
             requirements(Category.effect, with(XenItems.cobalt, 500, XenItems.iron, 500, Items.titanium, 350));
             health = 6400;
             size = 3;
@@ -47,4 +43,5 @@ public class XenStorage{
 
             researchCostMultiplier = 1f;
         }};
+    }
 }
