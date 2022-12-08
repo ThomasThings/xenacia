bde8f57366b6ed064f35571d40246d5179e5aa2e
ThomasThings <TheYTFurryGamer@gmail.com>
2022-12-08 02:22:58 -0500

    updated unitfactories and added names

--- src/xenacia/content/XenUnitFactories.java
+++ src/xenacia/content/XenUnitFactories.java
@@ -21,10 +21,10 @@ public class XenUnitFactories{
         miteFactory = new BaseTeamUnitFactory("mite-factory"){{
             requirements(Category.units, BuildVisibility.sandboxOnly, with(XenItems.iron, 50, Items.silicon, 40));
             plans = Seq.with(
+                    new UnitPlan(XenUnitTypes.coreMite, 60f * 8, with(Items.silicon, 5, XenItems.aluminum, 3)),
                     new UnitPlan(XenUnitTypes.assaultMite, 60f * 8, with(Items.silicon, 5, XenItems.iron, 3)),
                     new UnitPlan(XenUnitTypes.boltMite, 60f * 8, with(Items.silicon, 5, XenItems.iron, 2, XenItems.aluminum, 2)),
-                    new UnitPlan(XenUnitTypes.sapMite, 60f * 8, with(Items.silicon, 5, XenItems.aluminum, 3)),
-                    new UnitPlan(XenUnitTypes.coreMite, 60f * 8, with(Items.silicon, 5, XenItems.aluminum, 3))
+                    new UnitPlan(XenUnitTypes.sapMite, 60f * 8, with(Items.silicon, 5, XenItems.aluminum, 3))
             );
             size = 2;
             consumePower(0.8f);
@@ -32,11 +32,11 @@ public class XenUnitFactories{
         unitSeedConstructor = new BaseTeamUnitFactory("unit-seed-constructor"){{
             requirements(Category.units, with( XenItems.aluminum, 100, Items.silicon, 80, XenItems.iron, 80));
             plans = Seq.with(
-                    new UnitPlan(XenUnitTypes.mech, 60f * 15, with(Items.silicon, 10, XenItems.iron, 15, XenItems.aluminum, 5)),
-                    new UnitPlan(XenUnitTypes.aircraft, 60f * 15, with(Items.silicon, 10, XenItems.iron, 5, XenItems.aluminum, 10)),
-                    new UnitPlan(XenUnitTypes.polyped, 60f * 15, with(Items.silicon, 15, XenItems.iron, 10, XenItems.aluminum, 5)),
-                    new UnitPlan(XenUnitTypes.ship, 60f * 15, with(Items.silicon, 10, XenItems.aluminum, 15)),
-                    new UnitPlan(XenUnitTypes.tank, 60f * 15, with(Items.silicon, 10, XenItems.iron, 15, XenItems.aluminum, 10))
+                    new UnitPlan(XenUnitTypes.mech, 60f * 15, with(Items.silicon, 15, XenItems.iron, 15, XenItems.aluminum, 5)),
+                    new UnitPlan(XenUnitTypes.aircraft, 60f * 15, with(Items.silicon, 15, XenItems.iron, 5, XenItems.aluminum, 10)),
+                    new UnitPlan(XenUnitTypes.polyped, 60f * 15, with(Items.silicon, 20, XenItems.iron, 10, XenItems.aluminum, 5)),
+                    new UnitPlan(XenUnitTypes.ship, 60f * 15, with(Items.silicon, 20, XenItems.aluminum, 15)),
+                    new UnitPlan(XenUnitTypes.tank, 60f * 15, with(Items.silicon, 25, XenItems.iron, 15, XenItems.aluminum, 10))
             );
             size = 3;
             consumePower(1.2f);
@@ -46,11 +46,16 @@ public class XenUnitFactories{
 
             size = 3;
             consumePower(3f);
-            consumeItems(with(Items.silicon, 30, XenItems.iron, 35, XenItems.aluminum, 20));
+            consumeItems(with(Items.silicon, 60, XenItems.iron, 55, XenItems.aluminum, 25));
 
             constructTime = 60f * 10f;
 
             upgrades.addAll(
+                    new UnitType[]{XenUnitTypes.mech, XenUnitTypes.gale},
+                    new UnitType[]{XenUnitTypes.aircraft, XenUnitTypes.anax},
+                    new UnitType[]{XenUnitTypes.polyped, XenUnitTypes.explore},
+                    new UnitType[]{XenUnitTypes.ship, XenUnitTypes.shif},
+                    new UnitType[]{XenUnitTypes.tank, XenUnitTypes.tack}
             );
         }};
         supportTranstructor = new BaseTeamReconstructor("support-transtructor"){{
