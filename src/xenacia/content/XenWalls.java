884c6fc522f1b2c637d85a920b5d9abb3db24bec
ThomasThings <TheYTFurryGamer@gmail.com>
2022-12-08 01:52:59 -0500

    update wall hp and names and costs

--- src/xenacia/content/XenWalls.java
+++ src/xenacia/content/XenWalls.java
@@ -3,7 +3,6 @@ package xenacia.content;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.defense.*;
-import mindustry.world.meta.*;
 import mindustry.content.*;
 
 import static mindustry.type.ItemStack.*;
@@ -12,7 +11,7 @@ public class XenWalls{
     public static Block
             ironWall, ironWallLarge, bulkIronWall, bulkIronWallLarge, bulkIronWallHuge,
             graphiteWall, graphiteWallLarge, bulkGraphiteWall, bulkGraphiteWallLarge, bulkGraphiteWallHuge,
-            bulkTitaniumWall, bulkTitaniumWallLarge, bulkTitaniumWallHuge,
+            titaniumAlloyWall, titaniumAlloyWallLarge, bulkTitaniumAlloyWall, bulkTitaniumAlloyWallLarge, bulkTitaniumAlloyWallHuge,
             cobaltWall, cobaltWallLarge, bulkCobaltWall, bulkCobaltWallLarge, bulkCobaltWallHuge;
 
     public static void load(){
@@ -21,130 +20,124 @@ public class XenWalls{
             requirements(Category.defense, with(XenItems.iron, 6));
             health = 400;
             size = 1;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.iron, 100);
         }};
         ironWallLarge = new Wall("iron-wall-large"){{
             requirements(Category.defense, with(XenItems.iron, 6 * 4));
             health = 400 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.iron, 800);
         }};
         bulkIronWall = new Wall("bulk-iron-wall"){{
             requirements(Category.defense, with(XenItems.iron, 8 * 4, XenItems.aluminum, 4 * 4));
             health = 400 * 2 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.iron, 1200, XenItems.aluminum, 800);
         }};
         bulkIronWallLarge = new Wall("bulk-iron-wall-large"){{
             requirements(Category.defense, with(XenItems.iron, 8 * 9, XenItems.aluminum, 4 * 9));
             health = 400 * 2 * 9;
             size = 3;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.iron, 2000, XenItems.aluminum, 1000);
         }};
         bulkIronWallHuge = new Wall("bulk-iron-wall-huge"){{
             requirements(Category.defense, with(XenItems.iron, 8 * 16, XenItems.aluminum, 4 * 16));
             health = 400 * 2 * 16;
             size = 4;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.iron, 2000, XenItems.aluminum, 1000);
         }};
         //graphite
         graphiteWall = new Wall("graphite-wall"){{
-            requirements(Category.defense, with(Items.graphite, 4, XenItems.aluminum, 2));
-            health = 450;
+            requirements(Category.defense, with(Items.graphite, 6, XenItems.aluminum, 2));
+            health = 550;
             size = 1;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.graphite, 600, XenItems.aluminum, 300);
         }};
         graphiteWallLarge = new Wall("graphite-wall-large"){{
-            requirements(Category.defense, with(Items.graphite, 4 * 4, XenItems.aluminum, 2 * 4));
-            health = 450 * 4;
+            requirements(Category.defense, with(Items.graphite, 6 * 4, XenItems.aluminum, 2 * 4));
+            health = 550 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.graphite, 1500, XenItems.aluminum, 800);
         }};
         bulkGraphiteWall = new Wall("bulk-graphite-wall"){{
-            requirements(Category.defense, with(Items.graphite, 6 * 4, XenItems.iron, 6 * 4));
-            health = 450 * 2 * 4;
+            requirements(Category.defense, with(Items.graphite, 8 * 4, XenItems.iron, 4 * 4));
+            health = 550 * 2 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.graphite, 2000, XenItems.iron, 1000);
         }};
         bulkGraphiteWallLarge = new Wall("bulk-graphite-wall-large"){{
-            requirements(Category.defense, with(Items.graphite, 6 * 9, XenItems.iron, 6 * 9));
-            health = 450 * 2 * 9;
+            requirements(Category.defense, with(Items.graphite, 8 * 9, XenItems.iron, 4 * 9));
+            health = 550 * 2 * 9;
             size = 3;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.graphite, 2500, XenItems.iron, 1500);
         }};
         bulkGraphiteWallHuge = new Wall("bulk-graphite-wall-huge"){{
-            requirements(Category.defense, with(Items.graphite, 6 * 16, XenItems.iron, 6 * 16));
-            health = 450 * 2 * 16;
+            requirements(Category.defense, with(Items.graphite, 8 * 16, XenItems.iron, 4 * 16));
+            health = 550 * 2 * 16;
             size = 4;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.graphite, 3000, XenItems.iron, 2000);
         }};
-        //titanium
-        bulkTitaniumWall = new Wall("bulk-titanium-wall"){{
-            requirements(Category.defense, with(Items.titanium, 8 * 4, Items.graphite, 4 * 4));
-            health = 600 * 2 * 4;
+        //titanium alloy
+        titaniumAlloyWall = new Wall("titanium-alloy-wall"){{
+            requirements(Category.defense, with(Items.titanium, 6, Items.graphite, 2, XenItems.iron, 4));
+            health = 1050;
+            size = 1;
+            researchCost = with(Items.titanium, 600, Items.graphite, 300, XenItems.iron, 300);
+        }};
+        titaniumAlloyWallLarge = new Wall("titanium-alloy-wall-large"){{
+            requirements(Category.defense, with(Items.titanium, 6 * 4, Items.graphite, 2 * 4, XenItems.iron, 4 * 4));
+            health = 1050 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.titanium, 1500, Items.graphite, 800, XenItems.iron, 800);
         }};
-        bulkTitaniumWallLarge = new Wall("bulk-titanium-wall-large"){{
-            requirements(Category.defense, with(Items.titanium, 8 * 9, Items.graphite, 4 * 9));
-            health = 600 * 2 * 9;
+        bulkTitaniumAlloyWall = new Wall("bulk-titanium-alloy-wall"){{
+            requirements(Category.defense, with(Items.titanium, 8 * 4, Items.graphite, 4 * 4, XenItems.iron, 6 * 4));
+            health = 1050 * 2 * 4;
+            size = 2;
+            researchCost = with(Items.titanium, 2000, Items.graphite, 1000, XenItems.iron, 1000);
+        }};
+        bulkTitaniumAlloyWallLarge = new Wall("bulk-titanium-alloy-wall-large"){{
+            requirements(Category.defense, with(Items.titanium, 8 * 9, Items.graphite, 4 * 9, XenItems.iron, 6 * 9));
+            health = 1050 * 2 * 4;
             size = 3;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.titanium, 2500, Items.graphite, 1500, XenItems.iron, 1500);
         }};
-        bulkTitaniumWallHuge = new Wall("bulk-titanium-wall-huge"){{
-            requirements(Category.defense, with(Items.titanium, 8 * 16, Items.graphite, 4 * 16));
-            health = 600 * 2 * 16;
+        bulkTitaniumAlloyWallHuge = new Wall("bulk-titanium-alloy-wall-huge"){{
+            requirements(Category.defense, with(Items.titanium, 8 * 16, Items.graphite, 4 * 16, XenItems.iron, 6 * 16));
+            health = 1050 * 2 * 4;
             size = 4;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(Items.titanium, 3000, Items.graphite, 2000, XenItems.iron, 2000);
         }};
         //cobalt
         cobaltWall = new Wall("cobalt-wall"){{
-            requirements(Category.defense, with(XenItems.cobalt, 6));
-            health = 1000;
+            requirements(Category.defense, with(XenItems.cobalt, 8));
+            health = 1300;
             size = 1;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.cobalt, 1000);
         }};
         cobaltWallLarge = new Wall("cobalt-wall-large"){{
-            requirements(Category.defense, with(XenItems.cobalt, 6 * 4));
-            health = 1000 * 4;
+            requirements(Category.defense, with(XenItems.cobalt, 8 * 4));
+            health = 1300 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.cobalt, 1500);
         }};
         bulkCobaltWall = new Wall("bulk-cobalt-wall"){{
-            requirements(Category.defense, with(XenItems.cobalt, 8 * 4, Items.titanium, 4 * 4));
-            health = 1000 * 2 * 4;
+            requirements(Category.defense, with(XenItems.cobalt, 12 * 4, Items.titanium, 6 * 4));
+            health = 1300 * 2 * 4;
             size = 2;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.cobalt, 2500, Items.titanium, 1500);
         }};
         bulkCobaltWallLarge = new Wall("bulk-cobalt-wall-large"){{
-            requirements(Category.defense, with(XenItems.cobalt, 8 * 9, Items.titanium, 4 * 9));
-            health = 1000 * 2 * 9;
+            requirements(Category.defense, with(XenItems.cobalt, 12 * 9, Items.titanium, 6 * 9));
+            health = 1300 * 2 * 9;
             size = 3;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.cobalt, 3000, Items.titanium, 2000);
         }};
         bulkCobaltWallHuge = new Wall("bulk-cobalt-wall-huge"){{
-            requirements(Category.defense, with(XenItems.cobalt, 8 * 16, Items.titanium, 4 * 16));
-            health = 1000 * 2 * 16;
+            requirements(Category.defense, with(XenItems.cobalt, 12 * 16, Items.titanium, 6 * 16));
+            health = 1300 * 2 * 16;
             size = 4;
-            researchCostMultiplier = 0.1f;
-            envDisabled |= Env.scorching;
+            researchCost = with(XenItems.cobalt, 4000, Items.titanium, 3000);
         }};
     }
 }
