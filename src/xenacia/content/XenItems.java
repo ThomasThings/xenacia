e5ae1a84eb6bc655c88234e4a51c3815218f2c8d
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-02-18 02:13:27 -0500

    hm

--- src/xenacia/content/XenItems.java
+++ src/xenacia/content/XenItems.java
@@ -4,135 +4,158 @@ import arc.graphics.Color;
 import arc.struct.Seq;
 import mindustry.type.Item;
 
+import static mindustry.content.Items.*;
+
 public class XenItems{
     public static Item
-            aluminum, iron,
-            biomass, xenCoal, xenGraphite,
-            stone, xenSand, xenSilicon, lithium,
-            xenBeryllium, xenTitanium, cobalt,
-            amethyst, meteorFragment, polonium, neodymium,
-            xenTungsten, xenCarbide, volcrite, torrentAlloy;
+            aluminum, iron, stone, biomass, cobalt, metallicOxides, lithium, neodymium, chronatintilium, alterite,
+            ferricCompound, torrentAlloy, myaclide, gammasteel, braienasine, crystalineTherius, imparixFiber, spectraglass;
 
-    public static final Seq<Item> xenaciaItems = new Seq<>();
+    public static final Seq<Item> xenaciaItems = new Seq<>(), xenaciaOnlyItems = new Seq<>();
 
     public static void load(){
         aluminum = new Item("aluminum", Color.valueOf("bfbfcf")){{
-            hardness = 2;
+            hardness = 1;
             cost = 0.5f;
-            healthScaling = 0.1f;
+
+            alwaysUnlocked = true;
         }};
         iron = new Item("iron", Color.valueOf("a98b83")){{
-            hardness = 2;
-            cost = 0.5f;
-            healthScaling = 0.1f;
+            hardness = 1;
+            cost = 0.8f;
+        }};
+        stone = new Item("stone", Color.valueOf("666e77")){{
+            lowPriority = true;
+
+            buildable = false;
         }};
         biomass = new Item("biomass", Color.valueOf("283425")){{
-            flammability = 1.5f;
+            flammability = 1.4f;
 
-            hardness = 1;
             lowPriority = true;
+
             buildable = false;
         }};
-        /*xenCoal = new Item("xen-coal", Color.valueOf("2a2a2a")){{
-            explosiveness = 0.2f;
-            flammability = 1.2f;
+        cobalt = new Item("cobalt", Color.valueOf("515185")){{
+            hardness = 4;
+            cost = 1.1f;
 
-            hardness = 1;
-            buildable = false;
-        }};*/
-        /*xenGraphite = new Item("xen-graphite", Color.valueOf("8193b9")){{
+            healthScaling = 0.4f;
+        }};
+        metallicOxides = new Item("metallic-oxides", Color.valueOf("856c5f")){{
+            flammability = 0.15f;
+
+            cost = 0.6f;
+
+            healthScaling = -0.2f;
+        }};
+        lithium = new Item("lithium", Color.valueOf("9baa9d")){{
+            explosiveness = 0.25f;
+            flammability = 0.75f;
+            charge = 0.5f;
+
             hardness = 3;
             cost = 0.8f;
-            healthScaling = 0.2f;
-        }};*/
-        stone = new Item("stone", Color.valueOf("666e77")){{
-            hardness = 2;
-            lowPriority = true;
-            buildable = false;
+
+            healthScaling = 0f;
         }};
-        /*xenSand = new Item("xen-sand", Color.valueOf("f7cba4")){{
-            hardness = 1;
-            lowPriority = true;
-            buildable = false;
-        }};*/
-        /*xenSilicon = new Item("xen-silicon", Color.valueOf("48485d")){{
-            cost = 0.5f;
-            healthScaling = 0.15f;
-        }};*/
-        lithium = new Item("lithium", Color.valueOf("9baa9d")){{
-            explosiveness = 0.7f;
-            flammability = 0.6f;
+        neodymium = new Item("neodymium", Color.valueOf("2c2e34")){{
+            charge = 0.2f;
+
+            hardness = 4;
+
+            healthScaling = 0f;
+        }};
+        chronatintilium = new Item("chronatintilium", Color.valueOf("a8c7d0")){{
+            explosiveness = 1f;
+            radioactivity = 0.95f;
+            charge = 0.85f;
+
+            cost = 1.2f;
+
+            healthScaling = 0.2f;
+        }};
+        alterite = new Item("alternite", Color.valueOf("8f999b")){{
+            charge = 0.2f;
+
+            cost = 1.3f;
+
+            healthScaling = 0.2f;
+        }};
+        ferricCompound = new Item("ferric-compound", Color.valueOf("45484e")){{
+            explosiveness = 0.05f;
+            charge = 0.15f;
+
+            healthScaling = 0f;
+        }};
+        torrentAlloy = new Item("torrent-alloy", Color.valueOf("a1665a")){{
+            radioactivity = 0.7f;
+            charge = 0.35f;
+
+            cost = 1.5f;
+
+            healthScaling = 0.6f;
+        }};
+        myaclide = new Item("myaclide", Color.valueOf("588677")){{
+            radioactivity = 0.6f;
             charge = 0.4f;
 
-            hardness = 2;
-            cost = 0.8f;
-        }};
-        /*xenBeryllium = new Item("xen-beryllium", Color.valueOf("3f8b64")){{
-            hardness = 3;
-            cost = 1f;
-            healthScaling = 0.3f;
-        }};*/
-        /*xenTitanium = new Item("xen-titanium", Color.valueOf("8d9ae0")){{
-            hardness = 3;
-            cost = 1f;
-            healthScaling = 0.5f;
-        }};*/
-        cobalt = new Item("cobalt", Color.valueOf("3b3b69")){{
-            hardness = 4;
             cost = 1.2f;
+
             healthScaling = 0.8f;
         }};
-        amethyst = new Item("amethyst", Color.valueOf("713383")){{
-            hardness = 2;
-            buildable = false;
-        }};
-        meteorFragment = new Item("meteor-fragment", Color.valueOf("323940")){{
-            explosiveness = 0.05f;
-            radioactivity = 0.2f;
-            charge = 0.05f;
+        gammasteel = new Item("gammasteel", Color.valueOf("4f7497")){{
+            radioactivity = 0.3f;
+            charge = 1.5f;
 
-            hardness = 3;
-            buildable = false;
-        }};
-        polonium = new Item("polonium", Color.valueOf("a8c7d0")){{
-            explosiveness = 0.2f;
-            radioactivity = 1f;
+            cost = 2.2f;
 
-            hardness = 5;
-            cost = 1.5f;
-            healthScaling = 0.8f;
-        }};
-        neodymium = new Item("neodymium", Color.valueOf("424548")){{
-            hardness = 4;
-            cost = 1f;
-        }};
-        /*xenTungsten = new Item("xen-tungsten", Color.valueOf("534171")){{
-            hardness = 5;
-            cost = 1.5f;
-            healthScaling = 1.2f;
-        }};*/
-        /*xenCarbide = new Item("xen-carbide", Color.valueOf("534171")){{
-            hardness = 1;
-            cost = 1.5f;
-            healthScaling = 1.6f;
-        }};*/
-        volcrite = new Item("volcrite", Color.valueOf("483529")){{
-            hardness = 1;
-            cost = 1.5f;
-            hidden = true;
             healthScaling = 2f;
         }};
-        torrentAlloy = new Item("torrent-alloy", Color.valueOf("90524d")){{
-            explosiveness = 0.2f;
-            radioactivity = 0.4f;
-            charge = 0.8f;
+        braienasine = new Item("braienasine", Color.valueOf("788ac0")){{
+            explosiveness = 0.85f;
+            charge = 0.2f;
 
-            hardness = 1;
-            cost = 2f;
-            healthScaling = 2.5f;
+            cost = 1.3f;
+
+            healthScaling = 1f;
+        }};
+        crystalineTherius = new Item("crystaline-therius", Color.valueOf("c4c1ee")){{
+            explosiveness = 0.25f;
+            radioactivity = 0.3f;
+            charge = 0.4f;
+
+            cost = 1.5f;
+
+            healthScaling = 1.5f;
+        }};
+        imparixFiber = new Item("imparic-fiber", Color.valueOf("cf9666")){{
+            explosiveness = 0.1f;
+            flammability = 0.85f;
+            radioactivity = 0.5f;
+            charge = 0.2f;
+
+            cost = 1.6f;
+
+            healthScaling = 1.5f;
+        }};
+        spectraglass = new Item("spectraglass", Color.valueOf("d4d4f4")){{
+            radioactivity = 0.1f;
+            charge = 0.15f;
+
+            cost = 1.6f;
+
+            healthScaling = 1.8f;
         }};
 
 
-        xenaciaItems.addAll();
+        xenaciaItems.addAll(aluminum, iron, sand, stone, biomass, coal, graphite,
+                silicon, titanium, beryllium, cobalt, tungsten, metallicOxides,
+                lithium, neodymium, chronatintilium, alterite, ferricCompound,
+                carbide, torrentAlloy, myaclide, gammasteel, braienasine,
+                crystalineTherius, imparixFiber, spectraglass);
+
+        xenaciaOnlyItems.addAll(xenaciaItems).removeAll(serpuloItems);
+        xenaciaOnlyItems.removeAll(erekirItems);
     }
 }
--- src/xenacia/content/XenLiquids.java
+++ src/xenacia/content/XenLiquids.java
@@ -1,32 +1,107 @@
 package xenacia.content;
 
 import arc.graphics.Color;
+import arc.struct.ObjectSet;
+import mindustry.content.Fx;
+import mindustry.content.StatusEffects;
 import mindustry.type.Liquid;
 
 public class XenLiquids{
     public static Liquid
-            steam, liquidNitrogen, magma, volatlite;
+            magma, liquidNitrogen, volatlite, steam;
 
     public static void load(){
-        steam = new Liquid("fluid-steam", Color.valueOf("e9f8f9")){{
-            gas = true;
-            barColor = Color.valueOf("84e4ea");
+        magma = new Liquid("magma", Color.valueOf("db7448")){{
+            gasColor = Color.valueOf("db7448");
+            barColor = Color.valueOf("db7448");
+            lightColor = Color.valueOf("db7448");
+
+            flammability = 0;
+            temperature = 0.5f;
+            heatCapacity = 0.8f;
+            viscosity = 1f;
+            explosiveness = 0;
+
+            blockReactive = true;
+            coolant = false;
+            moveThroughBlocks = false;
+            incinerable = true;
+            effect = XenStatusEffects.magmaCoated;
+            particleEffect = Fx.fuelburn;
+            particleSpacing = 60f;
+            capPuddles = true;
+            vaporEffect = Fx.vapor;
+            hidden = false;
+            canStayOn = new ObjectSet<>();
         }};
-        liquidNitrogen = new Liquid("fluid-liquid-nitrogen", Color.valueOf("e4d6f6")){{
-            temperature = 0f;
-            viscosity = 0.2f;
+        liquidNitrogen = new Liquid("liquid-nitrogen", Color.valueOf("e4d6f6")){{
+            gas = false;
+            gasColor = Color.valueOf("db7448");
+            barColor = Color.valueOf("db7448");
+            lightColor = Color.valueOf("db7448");
+            flammability = 0;
+            temperature = 0.5f;
+            heatCapacity = 0.8f;
+            viscosity = 1f;
+            explosiveness = 1;
+            blockReactive = true;
+            coolant = false;
+            moveThroughBlocks = false;
+            incinerable = true;
+            effect = StatusEffects.none;
+            particleEffect = Fx.none;
+            particleSpacing = 60f;
+            boilPoint = 2f;
+            capPuddles = true;
+            vaporEffect = Fx.vapor;
+            hidden = false;
+            canStayOn = new ObjectSet<>();
         }};
-        magma = new Liquid("fluid-magma", Color.valueOf("db7448")){{
-            temperature = 3f;
-            viscosity = 0.8f;
-            incinerable = false;
-            gasColor = Color.valueOf("ffcb89");
+        volatlite = new Liquid("volatlite", Color.valueOf("7480c3")){{
+            gas = false;
+            gasColor = Color.valueOf("db7448");
+            barColor = Color.valueOf("db7448");
+            lightColor = Color.valueOf("db7448");
+            flammability = 0;
+            temperature = 0.5f;
+            heatCapacity = 0.8f;
+            viscosity = 1f;
+            explosiveness = 1;
+            blockReactive = true;
+            coolant = false;
+            moveThroughBlocks = false;
+            incinerable = true;
+            effect = StatusEffects.none;
+            particleEffect = Fx.none;
+            particleSpacing = 60f;
+            boilPoint = 2f;
+            capPuddles = true;
+            vaporEffect = Fx.vapor;
+            hidden = false;
+            canStayOn = new ObjectSet<>();
         }};
-        volatlite = new Liquid("fluid-volatlite", Color.valueOf("7480c3")){{
-           temperature = 0.6f;
-           flammability = 2f;
-           explosiveness = 3f;
-           viscosity = 0.2f;
+        steam = new Liquid("steam", Color.valueOf("e9f8f9")){{
+            gas = false;
+            gasColor = Color.valueOf("db7448");
+            barColor = Color.valueOf("db7448");
+            lightColor = Color.valueOf("db7448");
+            flammability = 0;
+            temperature = 0.5f;
+            heatCapacity = 0.8f;
+            viscosity = 1f;
+            explosiveness = 1;
+            blockReactive = true;
+            coolant = false;
+            moveThroughBlocks = false;
+            incinerable = true;
+            effect = StatusEffects.none;
+            particleEffect = Fx.none;
+            particleSpacing = 60f;
+            boilPoint = 2f;
+            capPuddles = true;
+            vaporEffect = Fx.vapor;
+            hidden = false;
+            canStayOn = new ObjectSet<>();
         }};
     }
 }
