37e68926843e7e3af374876391047a6af109a950
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-04-03 16:45:38 -0400

    GroundMinerAI?

--- src/xenacia/content/XenUnitTypes.java
+++ src/xenacia/content/XenUnitTypes.java
@@ -5,7 +5,7 @@ import arc.math.geom.Rect;
 import arc.struct.Seq;
 import mindustry.ai.UnitCommand;
 import mindustry.ai.types.BuilderAI;
-import mindustry.ai.types.MinerAI;
+import xenacia.ai.types.GroundMinerAI;
 import mindustry.gen.TankUnit;
 import mindustry.gen.UnitEntity;
 import mindustry.type.UnitType;
@@ -59,7 +59,7 @@ public class XenUnitTypes{
         awren = new UnitType("awren") {{
             constructor = TankUnit::create;
             outlineColor = Color.valueOf("40435a");
-            controller = u -> new MinerAI();
+            controller = u -> new GroundMinerAI();
             defaultCommand = UnitCommand.mineCommand;
             health = 350f;
             armor = 2f;
