2bf60bca8bb8ef04eb7af031f95783b9d2d7d164
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-05-16 01:19:15 -0400

    a

--- src/xenacia/content/XenEnvironmentTiles.java
+++ src/xenacia/content/XenEnvironmentTiles.java
@@ -15,6 +15,10 @@ public class XenEnvironmentTiles{
     public static Block
             lendesiteFloor, deepLendesiteFloor, slashedLendesiteFloor, deepSlashedLendesiteFloor, lendesiteWall, lendesiteBoulder,
             kryesenFloor, deepKryesenFloor, kryesenWall, kryesenBoulder,
+            asuralineFloor, deepAsuralineFloor, asuralineWall, asuralineBoulder,
+            asulariteFloor, deepAsulariteFloor, asulariteWall, asulariteBoulder,
+            dolstoneFloor, deepDolstoneFloor, dolstoneWall, dolstoneBoulder,
+            vidriteFloor, deepVidriteFloor, vidriteWall, vidriteBoulder,
             shallowHydrex, deepHydrex,
             lushGrass, wetLushGrass, lushGrassWall,
             silicateSandFloor, wetSilicateSand,
@@ -31,19 +35,19 @@ public class XenEnvironmentTiles{
         }};
         deepLendesiteFloor = new Floor("deep-lendesite-floor"){{
             variants = 4;
-            mapColor = Color.valueOf("4d5c6c");
+            mapColor = Color.valueOf("5a6877");
         }};
         slashedLendesiteFloor = new Floor("slashed-lendesite-floor"){{
             variants = 4;
-            mapColor = Color.valueOf("6c7b8b");
+            mapColor = Color.valueOf("5a6877");
         }};
         deepSlashedLendesiteFloor = new Floor("deep-slashed-lendesite-floor"){{
             variants = 4;
-            mapColor = Color.valueOf("5e6d7d");
+            mapColor = Color.valueOf("5a6877");
         }};
         lendesiteWall = new StaticWall("lendesite-wall"){{
             variants = 3;
-            mapColor = Color.valueOf("778797");
+            mapColor = Color.valueOf("5a6877");
         }};
         lendesiteBoulder = new Prop("lendesite-boulder"){{
             variants = 5;
@@ -59,16 +63,88 @@ public class XenEnvironmentTiles{
         }};
         deepKryesenFloor = new Floor("deep-kryesen-floor"){{
             variants = 4;
-            mapColor = Color.valueOf("6474a8");
+            mapColor = Color.valueOf("788ac0");
         }};
         kryesenWall = new StaticWall("kryesen-wall"){{
-            variants = 2;
-            mapColor = Color.valueOf("a0b6e3");
+            variants = 3;
+            mapColor = Color.valueOf("788ac0");
         }};
         kryesenBoulder = new Prop("kryesen-boulder"){{
             variants = 2;
             kryesenFloor.asFloor().decoration = this;
             deepKryesenFloor.asFloor().decoration = this;
+        }};
+
+        asuralineFloor = new Floor("asuraline-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("4e4067");
+        }};
+        deepAsuralineFloor = new Floor("deep-asuraline-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("4e4067");
+        }};
+        asuralineWall = new StaticWall("asuraline-wall"){{
+            variants = 3;
+            mapColor = Color.valueOf("4e4067");
+        }};
+        asuralineBoulder = new Prop("asuraline-boulder"){{
+            variants = 2;
+            asuralineFloor.asFloor().decoration = this;
+            deepAsuralineFloor.asFloor().decoration = this;
+        }};
+
+        asulariteFloor = new Floor("asularite-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("3a314b");
+        }};
+        deepAsulariteFloor = new Floor("deep-asularite-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("3a314b");
+        }};
+        asulariteWall = new StaticWall("asularite-wall"){{
+            variants = 3;
+            mapColor = Color.valueOf("3a314b");
+        }};
+        asulariteBoulder = new Prop("asularite-boulder"){{
+            variants = 2;
+            asulariteFloor.asFloor().decoration = this;
+            deepAsulariteFloor.asFloor().decoration = this;
+        }};
+
+        dolstoneFloor = new Floor("dolstone-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("23282d");
+        }};
+        deepDolstoneFloor = new Floor("deep-dolstone-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("23282d");
+        }};
+        dolstoneWall = new StaticWall("dolstone-wall"){{
+            variants = 3;
+            mapColor = Color.valueOf("23282d");
+        }};
+        dolstoneBoulder = new Prop("dolstone-boulder"){{
+            variants = 3;
+            dolstoneFloor.asFloor().decoration = this;
+            deepDolstoneFloor.asFloor().decoration = this;
+        }};
+
+        vidriteFloor = new Floor("vidrite-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("b95755");
+        }};
+        deepVidriteFloor = new Floor("deep-vidrite-floor"){{
+            variants = 4;
+            mapColor = Color.valueOf("b95755");
+        }};
+        vidriteWall = new StaticWall("vidrite-wall"){{
+            variants = 3;
+            mapColor = Color.valueOf("b95755");
+        }};
+        vidriteBoulder = new Prop("vidrite-boulder"){{
+            variants = 2;
+            vidriteFloor.asFloor().decoration = this;
+            deepVidriteFloor.asFloor().decoration = this;
         }};
 
         shallowHydrex = new Floor("shallow-hydrex"){{
