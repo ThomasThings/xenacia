96210da7c92918c06a97d7400458a734294ff7df
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-05-20 00:32:22 -0400

    turrets?

--- src/xenacia/content/XenTurrets.java
+++ src/xenacia/content/XenTurrets.java
@@ -2,12 +2,15 @@ package xenacia.content;
 
 import arc.graphics.Color;
 import mindustry.content.Fx;
+import mindustry.entities.bullet.BasicBulletType;
 import mindustry.entities.bullet.LaserBoltBulletType;
 import mindustry.entities.bullet.LaserBulletType;
 import mindustry.entities.part.RegionPart;
+import mindustry.entities.pattern.ShootAlternate;
 import mindustry.gen.Sounds;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
 import mindustry.world.blocks.defense.turrets.PowerTurret;
 import mindustry.world.draw.DrawTurret;
 
@@ -15,7 +18,7 @@ import static mindustry.type.ItemStack.with;
 
 public class XenTurrets{
     public static Block
-            bolt, beam;
+            bolt, beam, cast, streak;
 
     public static void load(){
         bolt = new PowerTurret("bolt"){{
@@ -101,6 +104,73 @@ public class XenTurrets{
 
             consumePower(2f);
             coolant = consumeCoolant(0.1f);
+        }};
+        cast = new ItemTurret("cast"){{
+            requirements(Category.turret, with(XenItems.alamex, 55));
+            outlineColor = Color.valueOf("353344");
+            size = 1;
+            ammo(
+                    XenItems.alamex,  new BasicBulletType(3f, 5){{
+                        width = 6f;
+                        height = 7.5f;
+                        lifetime = 30f;
+                        ammoMultiplier = 5;
+                    }}
+            );
+
+            recoil = 0.5f;
+            shootY = 3f;
+            reload = 10f;
+            range = 230;
+            shootCone = 15f;
+            ammoUseEffect = Fx.casing1;
+            health = 200;
+            inaccuracy = 8f;
+            rotateSpeed = 8f;
+            coolant = consumeCoolant(0.1f);
+
+            limitRange();
+        }};
+        streak = new ItemTurret("streak"){{
+            requirements(Category.turret, with(XenItems.alamex, 180, XenItems.torren, 125));
+            outlineColor = Color.valueOf("353344");
+            size = 2;
+            ammo(
+                    XenItems.alamex,  new BasicBulletType(4.5f, 15){{
+                        width = 7f;
+                        height = 9f;
+                        lifetime = 60f;
+                        ammoMultiplier = 1;
+                    }}
+            );
+
+            shoot = new ShootAlternate(6f);
+
+            recoils = 2;
+            drawer = new DrawTurret(){{
+                for(int i = 0; i < 2; i ++){
+                    int f = i;
+                    parts.add(new RegionPart("-barrel-" + (i == 0 ? "l" : "r")){{
+                        progress = PartProgress.recoil;
+                        recoilIndex = f;
+                        under = true;
+                        moveY = -2f;
+                    }});
+                }
+            }};
+
+            recoil = 0.5f;
+            shootY = 3f;
+            reload = 25f;
+            range = 230;
+            shootCone = 15f;
+            ammoUseEffect = Fx.casing1;
+            health = 600;
+            inaccuracy = 4f;
+            rotateSpeed = 8f;
+            coolant = consumeCoolant(0.1f);
+
+            limitRange();
         }};
     }
 }
