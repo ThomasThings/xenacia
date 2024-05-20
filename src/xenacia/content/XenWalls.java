a670e9e12ccc78a68d769e15953d6190b3c466d9
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-05-19 23:07:52 -0400

    walls

--- src/xenacia/content/XenWalls.java
+++ src/xenacia/content/XenWalls.java
@@ -9,27 +9,31 @@ import static mindustry.type.ItemStack.with;
 
 public class XenWalls{
     public static Block
-            torrenWall, torrenWallLarge,
-            placeholderWall, placeholderWallLarge, placeholderWallHuge, placeholderWallGigantic, placeholderWallColossol, placeholderWallMammoth, placeholderWallTitanic, placeholderWallGargantuar, placeholderWallAbsurd,
-            darkPlaceholderWall, darkPlaceholderWallLarge, darkPlaceholderWallHuge, darkPlaceholderWallGigantic, darkPlaceholderWallColossol, darkPlaceholderWallMammoth, darkPlaceholderWallTitanic, darkPlaceholderWallGargantuar, darkPlaceholderWallAbsurd;
+            torrenWall, torrenWallLarge, alamexWall, alamexWallLarge;
 
     public static void load(){
 
         int wallHealthMultiplier = 4;
 
-        torrenWall = new Wall("torren-wall"){{
-            requirements(Category.defense, with(XenItems.torren, 6));
+        alamexWall = new Wall("alamex-wall"){{
+            requirements(Category.defense, with(XenItems.alamex, 6));
             health = 125 * wallHealthMultiplier;
             researchCostMultiplier = 0.1f;
         }};
-        torrenWallLarge = new Wall("torren-wall-large"){{
+        alamexWallLarge = new Wall("alamex-wall-large"){{
             requirements(Category.defense, ItemStack.mult(torrenWall.requirements, 4));
             health = 125 * wallHealthMultiplier;
             researchCostMultiplier = 0.1f;
         }};
-        placeholderWall = new Wall("placeholder-wall"){{
-            requirements(Category.defense, with(XenItems.alamex, 1));
-            health = 5 * wallHealthMultiplier;
+        torrenWall = new Wall("torren-wall"){{
+            requirements(Category.defense, with(XenItems.torren, 6));
+            health = 200 * wallHealthMultiplier;
+            researchCostMultiplier = 0.1f;
+        }};
+        torrenWallLarge = new Wall("torren-wall-large"){{
+            requirements(Category.defense, ItemStack.mult(torrenWall.requirements, 4));
+            health = 200 * wallHealthMultiplier;
+            researchCostMultiplier = 0.1f;
         }};
     }
 }
