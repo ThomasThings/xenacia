c36349ba213fa056e47d8692a3f69e93906c5928
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-06-17 14:36:23 -0400

    the

--- src/xenacia/content/XenFactoryBlocks.java
+++ src/xenacia/content/XenFactoryBlocks.java
@@ -1,6 +1,5 @@
 package xenacia.content;
 
-import arc.graphics.Color;
 import mindustry.content.Fx;
 import mindustry.entities.effect.RadialEffect;
 import mindustry.gen.Sounds;
@@ -8,9 +7,8 @@ import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.production.GenericCrafter;
-import mindustry.world.draw.DrawDefault;
-import mindustry.world.draw.DrawFlame;
 import mindustry.world.draw.DrawMulti;
+import mindustry.world.draw.DrawRegion;
 
 import static mindustry.type.ItemStack.with;
 
@@ -26,17 +24,20 @@ public class XenFactoryBlocks{
                 effect = Fx.disperseTrail;
                 amount = 4;
                 rotationSpacing = 90f;
-                lengthOffset = 20f;
+                lengthOffset = -5f;
                 rotationOffset = 45f;
                 baseRotation = 90f;
             }};
 
-            outputItem = new ItemStack(XenItems.vrensteel, 1);
-            craftTime = 10f;
-            size = 2;
+            outputItem = new ItemStack(XenItems.vrensteel, 2);
+            craftTime = 120f;
+            size = 3;
             hasPower = true;
             hasLiquids = false;
-            drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("c6cef0")));
+            drawer = new DrawMulti(
+                    new DrawRegion("-bottom"),
+                    new DrawRegion("-hammer")
+            );
             ambientSound = Sounds.smelter;
             ambientSoundVolume = 0.07f;
 
