f94148bf93ffa2006c6cb9d51130002e201f4338
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2024-10-08 00:49:14 -0400

    Update XenStatusEffects.java

--- src/xenacia/content/XenStatusEffects.java
+++ src/xenacia/content/XenStatusEffects.java
@@ -11,34 +11,34 @@ public class XenStatusEffects{
 
         doubleclock = new StatusEffect("doubleclock"){{
             color = Color.valueOf("c4c1ee");
-            healthMultiplier = 2f;
-            speedMultiplier = 2f;
-            damageMultiplier = 2f;
+            healthMultiplier = 1.8f;
+            speedMultiplier = 1.5f;
+            damageMultiplier = 1.5f;
             reloadMultiplier = 2f;
             effect = Fx.overclocked;
         }};
         quadclock = new StatusEffect("quadclock"){{
             color = Color.valueOf("e3897a");
-            healthMultiplier = 4f;
-            speedMultiplier = 4f;
-            damageMultiplier = 4f;
+            healthMultiplier = 3f;
+            speedMultiplier = 2.5f;
+            damageMultiplier = 2.5f;
             reloadMultiplier = 4f;
             effect = Fx.overclocked;
         }};
         superclock = new StatusEffect("superclock"){{
             color = Color.valueOf("836eab");
             healthMultiplier = 8f;
-            speedMultiplier = 8f;
-            damageMultiplier = 8f;
+            speedMultiplier = 4f;
+            damageMultiplier = 4f;
             reloadMultiplier = 8f;
             effect = Fx.overclocked;
         }};
         hyperclock = new StatusEffect("hyperclock"){{
             color = Color.valueOf("71d299");
-            healthMultiplier = 15f;
-            speedMultiplier = 15f;
-            damageMultiplier = 15f;
-            reloadMultiplier = 15f;
+            healthMultiplier = 25f;
+            speedMultiplier = 8f;
+            damageMultiplier = 8f;
+            reloadMultiplier = 20f;
             effect = Fx.overclocked;
         }};
     }
